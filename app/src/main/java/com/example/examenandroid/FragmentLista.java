5f19bf87f438a8e76826068de72081eab98df7e4
michaelgarciam1 <mgarciamoreira@cifpfbmoll.eu>
2023-11-30 19:32:27 +0100

    imagenes en FragmentLista

--- app/src/main/java/com/example/examenandroid/FragmentLista.java
+++ app/src/main/java/com/example/examenandroid/FragmentLista.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,27 +18,32 @@ import java.util.ArrayList;
 
 public class FragmentLista extends Fragment {
 
+    int[] texto= new int[]{R.string.futbol,R.string.baloncesto,R.string.tenis,R.string.padel};
+    int[] imagenes= new int[]{R.drawable.futbol,R.drawable.logotipo,R.drawable.tenis,R.drawable.padel};
     private ListView mListView;
 
     public FragmentLista() {
         // Required empty public constructor
     }
 
+    private void crearVista(){
 
+
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View viewLista = inflater.inflate(R.layout.fragment_lista, container, false);
         mListView = viewLista.findViewById(R.id.listView);
-        ArrayList<String> deportes = new ArrayList<>();
-        deportes.add("Futbol");
-        deportes.add("Baloncesto");
-        deportes.add("Tenis");
-        deportes.add("Padel");
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, deportes);
+//        ArrayList<String> deportes = new ArrayList<>();
+//        deportes.add("Futbol");
+//        deportes.add("Baloncesto");
+//        deportes.add("Tenis");
+//        deportes.add("Padel");
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, deportes);
+        CustomAdapter adapter = new CustomAdapter();
         mListView.setAdapter(adapter);
-
         mListView.setClickable(true);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -99,5 +107,31 @@ public class FragmentLista extends Fragment {
         });
         return viewLista;
 
+    }
+
+    private class CustomAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return texto.length;
+        }
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View vista = getLayoutInflater().inflate(R.layout.fila_imagen, null);
+            TextView name = vista.findViewById(R.id.textoLista);
+            ImageView imagen = vista.findViewById(R.id.imagenLista);
+            name.setText(texto[i]);
+            imagen.setImageResource(imagenes[i]);
+            return vista;
+        }
     }
 }
