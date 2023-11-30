b13cf4a4a6a4518aa2f718fd7c58d373e0d2cc81
michaelgarciam1 <mgarciamoreira@cifpfbmoll.eu>
2023-11-30 19:01:14 +0100

    horizontal y vertical

--- app/src/main/java/com/example/examenandroid/FragmentLista.java
+++ app/src/main/java/com/example/examenandroid/FragmentLista.java
@@ -1,14 +1,15 @@
 package com.example.examenandroid;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 
@@ -19,7 +20,6 @@ public class FragmentLista extends Fragment {
     public FragmentLista() {
         // Required empty public constructor
     }
-
 
 
     @Override
@@ -35,6 +35,69 @@ public class FragmentLista extends Fragment {
         deportes.add("Padel");
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, deportes);
         mListView.setAdapter(adapter);
+
+        mListView.setClickable(true);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+
+                //Object o = listView.getItemAtPosition(position);
+                // Realiza lo que deseas, al recibir clic en el elemento de tu listView determinado por su posicion.
+//                Log.i("Click", "click en el elemento " + position + " de mi ListView");
+                if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    if (position == 0) {
+                        requireActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame1, new FragmentFutbol())
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                    if (position == 1) {
+                        requireActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame1, new FragmentBaloncesto())
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                    if (position == 2) {
+                        requireActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame1, new FragmentTenis())
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                    if (position == 3) {
+                        requireActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame1, new FragmentPadel())
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                } else {
+                    if (position == 0) {
+                        requireActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame2, new FragmentFutbol())
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                    if (position == 1) {
+                        requireActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame2, new FragmentBaloncesto())
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                    if (position == 2) {
+                        requireActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame2, new FragmentTenis())
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                    if (position == 3) {
+                        requireActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame2, new FragmentPadel())
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                }
+            }
+        });
         return viewLista;
+
     }
 }
--- app/src/main/java/com/example/examenandroid/MainActivity.java
+++ app/src/main/java/com/example/examenandroid/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.examenandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().beginTransaction().add(R.id.frame1, new FragmentLista()).commit();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame1, new FragmentLista())
+                    .addToBackStack(null)
+                    .commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame1, new FragmentLista())
+                    .addToBackStack(null)
+                    .commit();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame2, new FragmentFutbol())
+                    .addToBackStack(null)
+                    .commit();
+        }
+
     }
 }
